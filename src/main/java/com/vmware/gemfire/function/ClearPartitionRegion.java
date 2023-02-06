2f416b918aec15d21d30189ab6127f157e9b1ea3
pvermeulen-pivotal <pvermeulen.ctr@kr.af.mil>
2023-02-06 16:30:00 -0500

    updated to use latest gemfire

--- src/main/java/com/vmware/gemfire/function/ClearPartitionRegion.java
+++ src/main/java/com/vmware/gemfire/function/ClearPartitionRegion.java
@@ -1,30 +1,26 @@
-package io.pivotal.geode.function;
+package com.vmware.gemfire.function;
 
-import org.apache.geode.cache.Declarable;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.execute.Function;
 import org.apache.geode.cache.execute.FunctionContext;
 import org.apache.geode.cache.execute.FunctionException;
 import org.apache.geode.cache.execute.RegionFunctionContext;
 import org.apache.geode.cache.partition.PartitionRegionHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.Properties;
-
-public class ClearPartitionRegion implements Function, Declarable {
-	private static final Logger LOG = LoggerFactory.getLogger(ClearPartitionRegion.class);
+@Slf4j
+public class ClearPartitionRegion implements Function {
 
 	public void execute(FunctionContext context) {
-		if (LOG.isDebugEnabled())
-			LOG.debug(Thread.currentThread().getName() + " executing " + getId());
+		if (log.isDebugEnabled())
+			log.debug(Thread.currentThread().getName() + " executing " + getId());
 
 		if (context instanceof RegionFunctionContext) {
 			RegionFunctionContext rfc = (RegionFunctionContext) context;
 			Region localRegion = PartitionRegionHelper.getLocalDataForContext(rfc);
 			int numLocalEntries = localRegion.size();
 			localRegion.keySet().stream().forEach(key -> localRegion.remove(key));
-			LOG.info("Cleared " + numLocalEntries + " entries from " + localRegion.getName() + " region local data.");
+			log.info("Cleared " + numLocalEntries + " entries from " + localRegion.getName() + " region local data.");
 			context.getResultSender().lastResult(numLocalEntries);
 		} else {
 			throw new FunctionException("Context must be a RegionFunctionContext");
@@ -44,9 +40,6 @@ public class ClearPartitionRegion implements Function, Declarable {
 	}
 
 	public boolean isHA() {
-		return true;
-	}
-
-	public void init(Properties properties) {
+		return false;
 	}
 }
